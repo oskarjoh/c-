39f131dc296b2ae4899c86d2a847d658f4200726
oskar <oskar@mangosia.com>
2017-06-24 09:27:55 +0200

    lagt til en help del i koden

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,11 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string home;
             string yore_name;
             Console.WriteLine("skriv navnet dit");
            yore_name = Console.ReadLine();
             Console.WriteLine("hallo {0}", yore_name);
-
+            Console.WriteLine("hva vil du jøre nå {0}", yore_name);
+            home = Console.ReadLine();
+            while (true) { 
+                if (home == "help") {
+                    Console.WriteLine("welkomen til help");
+                    Console.WriteLine("her står alt det du trenger å vite om programet");
+                }
+            }
         }
     }
 }
