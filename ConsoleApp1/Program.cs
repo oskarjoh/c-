cdac948f242a6661508348a3c36f2a1642bf8317
oskar <oskar@mangosia.com>
2017-06-24 09:08:56 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yore_name;
+            int i = 0;
+            i = i+2;
+            Console.WriteLine(i.ToString());
+            Console.WriteLine("skriv navnet dit");
+           yore_name = Console.ReadLine();
+            Console.WriteLine("hallo {0}", yore_name);
+
+        }
+    }
+}
